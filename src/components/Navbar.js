d531240fa4d50b32b151f0814971b3f70cb1b626
wisdom <wisdomwarrie11@gmail.com>
2025-04-17 18:18:51 +0100

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top px-3">
-      <span className="navbar-brand fw-bold text-white"><button onClick={() => navigate('/')}>GirlieQuest</button></span>
+      <span className="navbar-brand fw-bold text-white"><button className="btn btn-outline-light btn-sm" onClick={() => navigate('/')}>GirlieQuest</button></span>
       <div className="ms-auto d-flex gap-3">
         <button className="btn btn-outline-light btn-sm" onClick={() => navigate('/levels')}>
           🔙 Back to Levels
